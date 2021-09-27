e46a9a6151e0839532a391077cb3b877bb4ad32b
Ricardo Leon Valencia <rvalencia765@gmail.com>
2021-09-26 22:09:27 -0500

    js added

---
+++ scripts/Projects.js
@@ -0,0 +1,44 @@
+import Project from "./classes/Project.js";
+
+var projects = [
+  new Project({
+    name: "BatataBit", 
+    description: "First layout of a landing page using HTML5 and CSS Flexbox.",
+    site: "https://levarick.github.io/BatataBit/",
+    code: "https://github.com/Levarick/BatataBit",
+    img: "./res/batatabit.png" }
+  ),
+  new Project({
+    name: "Modern Interior Landing Page", 
+    description: "Responsive landing page using HTML, CSS and JavaScript.",
+    site: "https://levarick.github.io/interior-consultant/",
+    code: "https://github.com/Levarick/interior-consultant",
+    img: "./res/Interior.png" }
+  ),
+  new Project({
+    name: "JS Basic Course", 
+    description: "Result of the Basic JavaScript Course, calculation of perimeters and volumes, percentages and measures of central tendency.",
+    site: "https://levarick.github.io/curso-practico-js/",
+    code: "https://github.com/Levarick/curso-practico-js/",
+    img: "./res/Interior.png" }
+  )
+]
+
+const projectsContainer = document.getElementById("projects");
+
+projects.map((projects) => {
+  const view = `  
+  <div class="project__card fadeUp-animation">
+    <img src="${projects.img}" alt="">
+    <div class="card__info">
+      <h3>${projects.name}</h3>
+      <p>${projects.description}</p>
+    </div>
+    <div class="buttons__container">
+      <a href="${projects.site}" target="blank_" class="button--block">See project</a>
+      <a href="${projects.code}" target="blank_" class="button--inline">See code</a>
+    </div>`
+    projectsContainer.insertAdjacentHTML('beforeend', view)
+  console.log(view);
+  return view;
+});
---
+++ scripts/classes/Project.js
@@ -0,0 +1,15 @@
+export default class Project {
+  constructor({
+    name,
+    site,
+    code,
+    img,
+    description
+  }) {
+    this.name = name;
+    this.site = site;
+    this.code = code;
+    this.img = img;
+    this.description = description;
+  }
+}
